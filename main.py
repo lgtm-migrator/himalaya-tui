6e0a4091b25ba2f903408adca89d556c707301fc
Jack Deng <dlwxxxdlw@gmail.com>
2022-05-18 09:08:48 +0800

    Update title
    Signed-off-by: Jack Deng <dlwxxxdlw@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from himalaya_tui import HimalayaTui
 
-
 if __name__ == "__main__":
-    HimalayaTui.run(log="textual.log")
+    HimalayaTui.run(title="Himalaya Tui", log="textual.log")
